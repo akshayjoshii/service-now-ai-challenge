55cd3c8b168954184fd993792a285a1e92d3d830
akshayjoshii <akshayjoshi56@gmail.com>
2023-09-15 01:37:07 +0200

    Implemented create_text_chunks method to peform strided abstractive summarization

--- src/utils/preprocess_utils.py
+++ src/utils/preprocess_utils.py
@@ -6,6 +6,9 @@ import string
 import swifter
 import pandas as pd
 
+from tqdm import tqdm
+from transformers import T5TokenizerFast, AutoTokenizer
+
 import nltk
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
@@ -93,3 +96,79 @@ def normalize_text(df:pd.DataFrame) -> pd.DataFrame:
     df = df[df['question2'] != '']
 
     return df
+
+
+def create_text_chunks(
+        long_text:str,
+        tokenizer_name_or_path:str="google/flan-t5-large"
+    ) -> list[str]:
+    """ 
+    This method acepts really long input text & creates non-overlapping 
+    chunks of text of length max_model_length. It also makes sure any sentence 
+    is not split in between when chunk length is reached. In such cases, the
+    sentence is added to the next chunk. 
+
+    :param long_text: Input text
+    :param tokenizer_name_or_path: Name or path of the tokenizer to be used. By 
+                                    default, it uses the tokenizer for T5 model.
+
+    :return: List of text chunks
+    """
+
+    try:
+        # If the user picks the default T5 tokenizer
+        tokenizer = T5TokenizerFast.from_pretrained(tokenizer_name_or_path)
+
+    except:
+        # When any other tokenizer is used
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
+
+    # Get the max length of the model
+    max_model_length = tokenizer.model_max_length
+    # print(f"Max seq. length of the model: {max_model_length}")
+
+    sentences = nltk.tokenize.sent_tokenize(long_text)
+
+    # initialize
+    length = 0
+    count = -1
+
+    # Temp string to store the chunk
+    chunk = ""
+
+    # List to store all the chunks
+    chunks = []
+
+    # Wrap using tqdm for progress bar
+    for sentence in tqdm(sentences):
+        count += 1
+
+        # Add the no. of sentence tokens to the length counter
+        combined_length = len(tokenizer.tokenize(sentence)) + length
+
+        # if it doesn't exceed the max length
+        if combined_length  <= tokenizer.max_len_single_sentence: 
+            chunk += sentence + " " # add the sentence to the chunk
+            length = combined_length # update the length counter
+
+            # if it is the last sentence
+            if count == len(sentences) - 1:
+                chunks.append(chunk) # save the chunk
+            
+        else: 
+            chunks.append(chunk) # save the chunk
+            
+            # Reset the counters for a new chunk 
+            length = 0 
+            chunk = ""
+
+            # Take care of the overflow sentence (i.e., sentence whose partial portion
+            # can only be saved in the previous chunk)
+            chunk += sentence + " "
+            length = len(tokenizer.tokenize(sentence))
+
+    return chunks
+
+
+if __name__ == "__main__":
+    create_text_chunks("Akshay Joshi" * 200)
